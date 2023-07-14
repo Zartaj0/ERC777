d7a08ec8491e496221683f7821c13be176ac1977
zartaj <zartaj.afsar@gmail.com>
2023-07-14 14:04:44 +0530

    upgrades

--- contracts/ERC777ERC20BaseToken.sol
+++ contracts/ERC777ERC20BaseToken.sol
@@ -3,10 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 pragma solidity ^0.8.0;
 
-
-import { ERC20Token } from "./ERC20Token.sol";
-import { ERC777BaseToken } from "./ERC777BaseToken.sol";
-
+import {ERC20Token} from "./ERC20Token.sol";
+import {ERC777BaseToken} from "./ERC777BaseToken.sol";
 
 contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
     bool internal mErc20compatible;
@@ -18,30 +16,68 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
         string memory _symbol,
         uint256 _granularity,
         address[] memory _defaultOperators
-    )
-        internal ERC777BaseToken(_name, _symbol, _granularity, _defaultOperators)
-    {
+    ) ERC777BaseToken(_name, _symbol, _granularity, _defaultOperators) {
         mErc20compatible = true;
-        setInterfaceImplementation("ERC20Token", address(this));
+        // setInterfaceImplementation("ERC20Token", address(this));
     }
 
     /// @notice This modifier is applied to erc20 obsolete methods that are
     ///  implemented only to maintain backwards compatibility. When the erc20
     ///  compatibility is disabled, this methods will fail.
-    modifier erc20 () {
+    modifier erc20() {
         require(mErc20compatible, "ERC20 is disabled");
         _;
     }
 
+    function name()
+        public
+        view
+        override(ERC777BaseToken, ERC20Token)
+        returns (string memory)
+    {
+        return mName;
+    }
+
+    /// @return the symbol of the token
+    function symbol()
+        public
+        view
+        override(ERC777BaseToken, ERC20Token)
+        returns (string memory)
+    {
+        return mSymbol;
+    }
+
+    /// @return the total supply of the token
+    function totalSupply()
+        public
+        view
+        override(ERC777BaseToken, ERC20Token)
+        returns (uint256)
+    {
+        return mTotalSupply;
+    }
+
+    function balanceOf(
+        address _tokenHolder
+    ) public view override(ERC777BaseToken, ERC20Token) returns (uint256) {
+        return mBalances[_tokenHolder];
+    }
+
     /// @notice For Backwards compatibility
     /// @return The decimals of the token. Forced to 18 in ERC777.
-    function decimals() public erc20 view returns (uint8) { return uint8(18); }
+    function decimals() public view erc20 returns (uint8) {
+        return uint8(18);
+    }
 
     /// @notice ERC20 backwards compatible transfer.
     /// @param _to The address of the recipient
     /// @param _amount The number of tokens to be transferred
-    /// @return `true`, if the transfer can't be done, it should fail.
-    function transfer(address _to, uint256 _amount) public erc20 returns (bool success) {
+    /// @return success `true`, if the transfer can't be done, it should fail.
+    function transfer(
+        address _to,
+        uint256 _amount
+    ) public erc20 returns (bool success) {
         doSend(msg.sender, msg.sender, _to, _amount, "", "", false);
         return true;
     }
@@ -50,12 +86,19 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
     /// @param _from The address holding the tokens being transferred
     /// @param _to The address of the recipient
     /// @param _amount The number of tokens to be transferred
-    /// @return `true`, if the transfer can't be done, it should fail.
-    function transferFrom(address _from, address _to, uint256 _amount) public erc20 returns (bool success) {
-        require(_amount <= mAllowed[_from][msg.sender], "Not enough funds allowed");
+    /// @return success `true`, if the transfer can't be done, it should fail.
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public erc20 returns (bool success) {
+        require(
+            _amount <= mAllowed[_from][msg.sender],
+            "Not enough funds allowed"
+        );
 
         // Cannot be after doSend because of tokensReceived re-entry
-        mAllowed[_from][msg.sender] = mAllowed[_from][msg.sender].sub(_amount);
+        mAllowed[_from][msg.sender] = mAllowed[_from][msg.sender]-_amount;
         doSend(msg.sender, _from, _to, _amount, "", "", false);
         return true;
     }
@@ -64,8 +107,10 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
     ///  `msg.sender` approves `_spender` to spend `_amount` tokens on its behalf.
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _amount The number of tokens to be approved for transfer
-    /// @return `true`, if the approve can't be done, it should fail.
-    function approve(address _spender, uint256 _amount) public erc20 returns (bool success) {
+    function approve(
+        address _spender,
+        uint256 _amount
+    ) public erc20 returns (bool success) {
         mAllowed[msg.sender][_spender] = _amount;
         emit Approval(msg.sender, _spender, _amount);
         return true;
@@ -75,9 +120,12 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
     ///  This function makes it easy to read the `allowed[]` map
     /// @param _owner The address of the account that owns the token
     /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens of _owner that _spender is allowed
+    /// @return remaining Amount of remaining tokens of _owner that _spender is allowed
     ///  to spend
-    function allowance(address _owner, address _spender) public erc20 view returns (uint256 remaining) {
+    function allowance(
+        address _owner,
+        address _spender
+    ) public view erc20 returns (uint256 remaining) {
         return mAllowed[_owner][_spender];
     }
 
@@ -89,11 +137,19 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
         bytes memory _data,
         bytes memory _operatorData,
         bool _preventLocking
-    )
-        internal
-    {
-        super.doSend(_operator, _from, _to, _amount, _data, _operatorData, _preventLocking);
-        if (mErc20compatible) { emit Transfer(_from, _to, _amount); }
+    ) internal override {
+        super.doSend(
+            _operator,
+            _from,
+            _to,
+            _amount,
+            _data,
+            _operatorData,
+            _preventLocking
+        );
+        if (mErc20compatible) {
+            emit Transfer(_from, _to, _amount);
+        }
     }
 
     function doBurn(
@@ -102,10 +158,10 @@ contract ERC777ERC20BaseToken is ERC20Token, ERC777BaseToken {
         uint256 _amount,
         bytes memory _data,
         bytes memory _operatorData
-    )
-        internal
-    {
+    ) internal override {
         super.doBurn(_operator, _tokenHolder, _amount, _data, _operatorData);
-        if (mErc20compatible) { emit Transfer(_tokenHolder, address(0), _amount); }
+        if (mErc20compatible) {
+            emit Transfer(_tokenHolder, address(0), _amount);
+        }
     }
 }
